7ff7fbce19e9d54c14b56913d6fdcbff80527459
AndreHammel <hammel.engineer@gmail.com>
2022-04-04 11:02:33 -0300

    correçao ortografica - linha 126 do arquivo /register/index.js de novo

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -123,7 +123,7 @@ export default function Register() {
             sx={{ width: '100%' }}
           >
             { firstWarning 
-              ? 'CPF/CNPJ deve conter somente números.' 
+              ? 'CPF/CNPJ deve conter somente números!' 
               : secondWarning 
                 ? 'CPF/CNPJ inválido.' 
                 : thirdWarning
